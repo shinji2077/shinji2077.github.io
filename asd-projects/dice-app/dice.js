b456a1ec3716b67d582fb97681780bd8880c3610
Axel J Mejia <shinjifreak2077@gmail.com>
2026-03-05 15:34:03 +0000

    Finished Walker project

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
   // Your code goes here
+  function makeDot(top, left, elementID){
+  $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementID);
+  }
 
+function rollDie(dieId) {
+  $(dieId).empty();
+
+  var randomNum = Math.ceil(Math.random() * 6);
+  console.log(randomNum);
+  
+  if (randomNum === 1) {
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, dieID); // top left
+  makeDot(75, 75, dieID); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, dieID); // top right
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(50, 50, dieID); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, dieID); // middle 
+  makeDot(75, 75, dieID); // bottom right
+  makeDot(25, 25, dieID); // top left
+  makeDot(25, 75, dieID); // bottom left
+  makeDot(75, 25, dieID); // top right
+}
+
+}
+
+function handleClick() {
+    rollDie("#die");
+}
+
+// Call it to test
+$("#die").on("click", handleClick);
 });
--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -6,7 +6,19 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+ const KEY = {
+  ENTER: 13,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+ };
+ let walker = {
+  x: 0,
+  y: 0,
+  speedX: 0,
+  speedY: 0
+ };
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
@@ -23,7 +35,9 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
+  $(document).on('keydown', handleKeyDown); 
+  $(document).on("keyup", handleKeyUp);
+           
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +48,9 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem();
+    redrawGameItem();
+    wallCollision(walker, "#walker");
   }
   
   /* 
@@ -44,15 +59,72 @@ function runProgram(){
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
 
+  if (event.which === KEY.LEFT) {
+    walker.speedX = -5
+    console.log("left pressed");
+  }
+  else if (event.which === KEY.UP ) {
+    walker.speedY = -5
+    console.log("up pressed");
+  }
+  else if (event.which === KEY.RIGHT) {
+    walker.speedX = 5
+    console.log("right pressed");
+  }
+   else if (event.which === KEY.DOWN) {
+    walker.speedY = 5
+    console.log("down pressed");
+   }
   }
 
+  function handleKeyUp(event) {
+  if (event.which === KEY.RIGHT || event.which === KEY.LEFT) {
+    walker.speedX = 0;
+  }
+
+  if (event.which === KEY.UP || event.which === KEY.DOWN) {
+    walker.speedY = 0;
+  }
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+function repositionGameItem() {
+  walker.x = walker.x + walker.speedX;
+  walker.y = walker.y + walker.speedY;
+}
   
+function redrawGameItem() {
+  $("#walker").css("left", walker.x + "px");
+  $("#walker").css("top", walker.y + "px");
+}
+
+
+function wallCollision(player, selector) {
+var boardWidth = $("#board").width()
+var boardHeight = $("#board").height()
+var playerWidth = $(selector).width()
+var playerHeight = $(selector).height()
+
+if (player.x < 0) {
+  player.x -= player.speedX 
+}
+
+if (player.x > boardWidth - playerWidth) {
+   player.x -= player.speedX
+    }
+
+if (player.y < 0) {
+    player.y -= player.speedY
+    }
+
+if (player.y > boardHeight - playerHeight) {
+    player.y -= player.speedY
+    }
+}
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
